aefbc8cd2ae43c666536b906f81d8052be636687
Mark Biek <support@janustech.net>
2013-06-18 12:10:20 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <meta name="google-site-verification" content="mfIzwtJqSTSIsAAG3Ps1z6Z7VhQYgHPIlVKrXy94CP8" />
+    <meta name="robots" content="noindex, nofollow, noarchive" />
+    <meta name="googlebot" content="noindex, nofollow, noarchive" />
+    <style type="text/css">
+        html, body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+
+    svg {
+      display:block; position:absolute;
+      top:5%; left:5%; width:90%; height:90%; background:#fff;
+    }
+    </style>
+</head>
+
+<body>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     viewBox="-150 -150 700 500" xml:space="preserve">
+    <ellipse fill="#FFFFFF" stroke="#000000" stroke-width="10px" cx="218.39" cy="85.292" rx="72.124" ry="73.009"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="14.159" y1="12.284" x2="14.159" y2="158.301"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="116.814" y1="12.284" x2="116.814" y2="158.301"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="14.159" y1="12.284" x2="65.486" y2="87.801"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="116.814" y1="12.284" x2="65.486" y2="87.801"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="218.39" y1="12.284" x2="175.392" y2="143.911"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="175.392" y1="143.911" x2="288.434" y2="67.837"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="288.434" y1="67.837" x2="146.266" y2="67.837"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="146.266" y1="67.837" x2="261.391" y2="143.911"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="261.391" y1="143.911" x2="218.39" y2="12.284"/>
+    <ellipse fill="#FFFFFF" stroke="#000000" stroke-width="10px" cx="375.434" cy="87.801" rx="72.124" ry="73.008"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="375.434" y1="14.792" x2="332.436" y2="146.42"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="332.436" y1="146.42" x2="445.478" y2="70.346"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="445.478" y1="70.346" x2="303.31" y2="70.346"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="303.31" y1="70.346" x2="418.435" y2="146.42"/>
+    <line fill="none" stroke="#000000" stroke-width="10px" x1="418.435" y1="146.42" x2="375.434" y2="14.792"/>
+</svg>
+</body>
+</html>
